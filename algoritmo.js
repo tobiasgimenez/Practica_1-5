ac9386b83c33dc3041adf4cb16c6f33bc6201473
tobias gimenez~ <tobiasgimenezchirino@gmail.com~>
2022-04-12 00:05:21 -0300

    agregue pasos para mi algoritmo, ademas una imagen y tambien un algoritmo.js

---
+++ algoritmo.js
@@ -0,0 +1,18 @@
+document.write(`
+-Agarrar la licuadora, 1 vaso, un cuchillo y una tabla
+    -Apartar 2 bananas, medio litro de leche, 50 gr de azucar y una cubetera de hielo
+    -Agarrar la licuadora y conectarla al enchufe
+    -Colocar 8 hielos adentro de la licuadora
+    -Agarrar el cuchillo y la tabla; cortar las 2 bananas
+    -Poner los trozos de banana en la licuadora
+    -Verter la azucar dentro de la licuadora
+    -Volcar el medio litro de leche en la licuadora
+    -Tapar la licuadora
+    -Prender la licuadora
+    -Licuar 2 minutos la mezcla
+    -Parar la licuadora
+    -Sacar la tapa 
+    -Agarrar el vaso
+    -Verter la mezcla dentro del vaso
+    -Desenchufar la licuadora
+`)
